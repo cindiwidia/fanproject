8fd112454b511c475390e0e7867db736f8e00afe
cindiwidia <cindiwidia913@gmail.com>
2023-10-14 20:07:51 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.gaybq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
@@ -10,32 +20,22 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
-@app.route("/bucket", methods=["POST"])
-def bucket_post():
-    bucket_receive = request.form['bucket_give']
-    count = db.bucket.count_documents({})
-    num = count + 1
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    # sample_receive = request.form['sample_give']
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
     doc = {
-        'num' : num,
-        'bucket' : bucket_receive,
-        'done' : 0
+        'name' : name_receive,
+        'comment' : comment_receive,
     }
-    db.bucket.insert_one(doc)
-    return jsonify({'msg': 'data saved!'})
+    db.fanmessages.insert_one(doc)
+    return jsonify({'msg': 'POST request!'})
 
-@app.route("/bucket/done", methods=["POST"])
-def bucket_done():
-    num_receive = request.form['num_give']
-    db.bucket.update_one(
-        { 'num': int(num_receive) },
-        { '$set': {'done': 1 } }
-    )
-    return jsonify({'msg': 'Update done!'})
-
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-    buckets_list = list(db.bucket.find({}, {'_id': False}))
-    return jsonify({'bucket': 'buckets_list'})
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    messages_list = list(db.fanmessages.find({}, {'_id': False}))
+    return jsonify({'messages': 'message_list'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
